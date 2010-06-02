d96c17fbdfbab9c26a38bd03b7aa8633b98e533f
Simon Hafner <hafnersimon@gmail.com>
2010-06-02 22:55:03 +0200

    implemented StringNode

---
+++ lib/resource_router/tree/string_node.rb
@@ -0,0 +1,28 @@
+require 'resource_router/tree/abstract_node'
+module ResourceRouter
+  module Tree
+    class StringNode < AbstractNode
+
+      # @param [Node] parent @see AbstractNode
+      # @param [String] string The String to be matched by this Node.
+      def initialize(parent, string)
+        super(parent)
+        @string = @name = string
+      end
+
+      # @return [String] The string this Node should match.
+      attr_reader :string
+
+      # @see AbstractNode#visit
+      def visit(runner)
+        runner.remaining_paths.shift
+      end
+
+      # @see AbstractNode#matches?
+      def matches?(runner)
+        runner.remaining_paths.first == string
+      end
+
+    end
+  end
+end
---
+++ test/suite/lib/tree/string_node.rb
@@ -0,0 +1,50 @@
+require 'resource_router/tree/string_node'
+suite "ResourceRouter" do
+  suite "Tree" do
+    suite "StringNode" do
+      setup do
+        @node_class = ResourceRouter::Tree::StringNode
+      end
+
+      exercise "a new StringNode" do
+        @node_class.new(:parent, :string)
+      end
+      verify "it's got the string set" do
+        equal(:string, returned.string)
+      end
+
+      suite "methods" do
+        setup do
+          @node = @node_class.new(:parent, "foo")
+          @runner = ResourceRouter::Runner.new(:node, %w(foo bar baz), %w())
+        end
+
+        exercise "#visit" do
+          @node.visit(@runner)
+        end
+        verify "the paths got shifted" do
+          equal(%w(bar baz), @runner.remaining_paths)
+        end
+
+        suite "#matches?" do
+
+          setup :runner, "a matching" do
+            @matches = true
+          end
+          setup :runner, "a not matching" do
+            @runner.remaining_paths.shift
+            @matches = false
+          end
+
+          exercise "#matches?" do
+            @node.matches?(@runner)
+          end
+          verify "" do
+            equal(@matches)
+          end
+
+        end
+      end
+    end
+  end
+end
