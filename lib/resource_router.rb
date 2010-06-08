8778f344a9606c32c8f436e6fb087c3424bc0434
Simon Hafner <hafnersimon@gmail.com>
2010-06-08 10:13:34 +0200

    added a bunch of requires

--- lib/resource_router.rb
+++ lib/resource_router.rb
@@ -1,2 +1,4 @@
+require 'resource_router/walker'
+require 'resource_router/tree'
 module ResourceRouter
 end
---
+++ lib/resource_router/tree.rb
@@ -0,0 +1,10 @@
+require 'resource_router/tree/node_set/abstract_node_set'
+require 'resource_router/tree/abstract_node'
+require 'resource_router/tree/glob_node'
+require 'resource_router/tree/root_node'
+require 'resource_router/tree/string_node'
+require 'resource_router/tree/variable_node'
+module ResourceRouter
+  module Tree
+  end
+end
